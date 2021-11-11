bbc32b8a1e42873f34e25a99c67ff4d75e437abc
Laure Linn <laure.linn@yahoo.com>
2021-11-10 20:34:54 -0800

    Added image gif and video player

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,30 @@
+import close_eyes from './close-your-eyes-close.gif';
+import globe from './Rotating_earth_(large).gif';
 import React from 'react';
 
 function App() {
   return (
-    <div>App</div>
+    <div class='ui active modal'>
+      <div class='header'>Header</div>
+      <div class='image content'>
+        <iframe
+          src='https://www.youtube.com/embed/eGRRvv0YOXk?rel=0&modestbranding=1&autohide=1&mute=1&showinfo=0&controls=0&autoplay=1'
+          width='560'
+          height='315'
+          frameborder='0'
+          allowfullscreen
+        ></iframe>
+        <img
+          class='image'
+          src={close_eyes}
+          alt='Italian Trulli'
+          width='200'
+        ></img>
+        <div class='description'>
+          <p></p>
+        </div>
+      </div>
+    </div>
   );
 }
 
